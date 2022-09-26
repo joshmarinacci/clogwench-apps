2645798f709c93f7b241a184b3bbf5514d51a989
Josh Marinacci <joshua@marinacci>
2022-09-26 10:30:06 -0700

    add simple-browser app

---
+++ simple-browser/src/index.ts
@@ -0,0 +1,119 @@
+import {
+    ActionButton,
+    BaseParentView, BaseView, CheckButton,
+    COMMAND_ACTION,
+    COMMAND_CHANGE, CoolEvent, DebugLayer, FillChildPanel,
+    HBox, HSpacer, KEYBOARD_CATEGORY, KEYBOARD_DOWN, KeystrokeCaptureView,
+    Label, Point,
+    POINTER_CATEGORY, POINTER_DOWN, POINTER_DRAG,
+    Rect, ScrollView,
+    Size,
+    SurfaceContext,
+    TextLine, ToggleButton,
+    VBox,
+    KeyboardEvent,
+    PointerEvent,
+    View, with_props,
+} from "thneed-gfx";
+import {App,ClogwenchWindowSurface, DBObj} from "thneed-idealos-common"
+import {BlockStyle, Paragraph, RichTextArea, TextStyle} from "./richtext";
+
+let plain:TextStyle = {
+    font: "base",
+    underline: false,
+    color:'black',
+    weight:'plain'
+}
+let link:TextStyle = {
+    font:"base",
+    underline: true,
+    color: 'blue',
+    weight:'plain',
+}
+let bold:TextStyle = {
+    font: "base",
+    underline: false,
+    color:'black',
+    weight:'bold'
+}
+let block_plain:BlockStyle = {
+    background_color: "white",
+    border_width: 0,
+    border_color: "black",
+    padding_width: 5,
+}
+let block_header:BlockStyle = {
+    background_color: 'cyan',
+    border_width: 1,
+    border_color: "#444444",
+    padding_width: 10
+}
+let DOC:Paragraph[] = [
+    {
+        runs:[
+            {
+                text:"This is some very cool and long text to read that will definitely need to be wrapped.",
+                style: plain,
+            },
+            {
+                text:"And this is some more text to read, now in BOLD!",
+                style: bold
+            },
+        ],
+        style: block_plain,
+    },
+    {
+        runs:[
+            {
+                text:"In the second paragraph.", style: plain
+            },
+            {
+                text: "Text can be underlined too.", style: link
+            }
+        ],
+        style: block_plain,
+    },
+    {
+        runs:[
+            {
+                text:"Third paragraph just has a single run of text in it.",
+                style: plain
+            }
+        ],
+        style: block_header,
+    }
+]
+
+
+function make_main_view(surface, app):View {
+    let scroll = new ScrollView();
+    scroll.set_hflex(true)
+    scroll.set_vflex(true)
+    scroll.set_content(with_props(new RichTextArea(), {doc:DOC}))
+    scroll.set_content(with_props(new ActionButton(),{caption:"button here"}))
+    return scroll
+}
+
+function start(app: App, surface: ClogwenchWindowSurface) {
+    let music_root = make_main_view(surface, app);
+    surface.set_root(music_root)
+    surface.start_input()
+    surface.repaint()
+}
+
+
+async function doit() {
+    console.log("making an app")
+    let app = new App()
+    await app.connect()
+    await app.send_and_wait({AppConnect: {HelloApp: {}}})
+    let win = await app.open_window(new Rect(50, 50, 600, 300))
+    let surface = new ClogwenchWindowSurface(win);
+    start(app,surface)
+    // app.on_close_window(() => {
+    //     console.log("window closed. quitting")
+    //     process.exit(0)
+    // })
+}
+
+doit().then(() => console.log("fully started")).catch((e) => console.error(e))
---
+++ simple-browser/src/richtext.ts
@@ -0,0 +1,329 @@
+import {
+    BaseView,
+    CoolEvent, Point,
+    POINTER_MOVE,
+    PointerEvent, Rect, Size, SurfaceContext
+} from "thneed-gfx"
+
+export type TextStyle = {
+    font: string,
+    color:string,
+    weight:string,
+    underline:boolean
+}
+export type BlockStyle = {
+    background_color: string,
+    border_width: number,
+    border_color: string,
+    padding_width: number,
+}
+
+type TextRun = {
+    text: string,
+    style: TextStyle,
+}
+export type Paragraph = {
+    runs: TextRun[]
+    style: BlockStyle,
+}
+
+
+type SpanBox = {
+    type: string,
+    position: Point,
+    text: string,
+    style: TextStyle,
+}
+
+type LineBox = {
+    type: string,
+    size: Size,
+    position: Point,
+    spans: SpanBox[],
+}
+type BlockBox = {
+    type: string,
+    size: Size,
+    position: Point,
+    style: BlockStyle,
+    lines: LineBox[],
+}
+type RootBox = {
+    type: string,
+    size: Size,
+    position: Point,
+    style: BlockStyle,
+    blocks: BlockBox[],
+}
+type ItRes = {
+    value: any,
+    done: boolean,
+}
+
+class WhitespaceIterator {
+    n: number
+    private text: string;
+    private done: boolean;
+
+    constructor(text: string) {
+        this.n = 0;
+        this.text = text;
+        this.done = false
+    }
+
+    next(): ItRes {
+        if (this.done) return {value: null, done: true}
+        let chunk = ""
+        while (true) {
+            let ch = this.text[this.n]
+            this.n++
+            if (this.n > this.text.length) {
+                this.done = true
+                return {
+                    value: chunk,
+                    done: false
+                }
+            }
+            if (ch === ' ') {
+                return {
+                    value: chunk,
+                    done: false
+                }
+            } else {
+                chunk += ch
+            }
+        }
+    }
+
+}
+
+function make_line_box(txt: string, w: number, h: number, pt: Point, style:TextStyle): LineBox {
+    let line: LineBox = {
+        type: "line",
+        position: pt.clone(),
+        size: new Size(w, h),
+        spans: [],
+    }
+    let span: SpanBox = {
+        style: style,
+        position: new Point(0, h),
+        text: txt,
+        type: "span",
+    }
+    line.spans.push(span)
+    return line
+}
+
+function do_layout(doc: Paragraph[], size: Size, g: SurfaceContext) {
+    let root_style:BlockStyle = {
+        background_color: 'white',
+        border_width: 0,
+        border_color: 'black',
+        padding_width: 5,
+    }
+    let root: RootBox = {
+        style: root_style,
+        blocks: [],
+        position: new Point(root_style.padding_width, root_style.padding_width),
+        size: new Size(size.w - root_style.padding_width * 2, size.h - root_style.padding_width * 2),
+        type: "root"
+    }
+
+    let y = root_style.padding_width
+    let x = root_style.padding_width
+    doc.forEach(para => {
+        let block: BlockBox = {
+            style: para.style,
+            lines: [],
+            position: new Point(x, y),
+            size: new Size(root.size.w - para.style.padding_width * 2, 100),
+            type: "block",
+        }
+        let line_height = 20
+
+        let curr_text = ""
+        let curr_pos = new Point(para.style.padding_width, para.style.padding_width)
+        let curr_w = 0
+        let avail_w = block.size.w
+
+        para.runs.forEach((run: TextRun) => {
+            let font = (run.style.weight === 'bold')?'bold':'base'
+            let chunks = new WhitespaceIterator(run.text)
+            let res = chunks.next()
+            while (res.done === false) {
+                let m = g.measureText(res.value, font)
+                if (curr_pos.x + curr_w + m.w < avail_w) {
+                    curr_text += ' ' + res.value
+                    curr_w += m.w + g.measureText(" ", font).w
+                } else {
+                    let line = make_line_box(curr_text, curr_w, line_height, curr_pos, run.style)
+                    block.lines.push(line)
+                    curr_text = res.value
+                    curr_w = m.w
+                    curr_pos.x = para.style.padding_width
+                    curr_pos.y += line_height
+                }
+                res = chunks.next()
+            }
+            if (curr_w > 0) {
+                block.lines.push(make_line_box(curr_text, curr_w, line_height, curr_pos, run.style))
+                curr_text = ""
+                curr_pos.x = curr_w
+                curr_w = 0
+            }
+        })
+        block.size.h = curr_pos.y + line_height + para.style.padding_width*2
+        root.blocks.push(block)
+        y = block.position.y + block.size.h + root_style.padding_width
+    })
+    if(y > root.size.h) {
+        root.size.h = y
+    }
+
+    return root
+}
+
+class JLogger {
+    private _indent: number;
+    constructor() {
+        this._indent = 0
+    }
+    l(...args) {
+        this.log(...args)
+    }
+
+    private log(...param: any[]) {
+        console.log(this.tab(),...param)
+    }
+
+    private tab() {
+        let str = ": "
+        for(let i=0; i<this._indent; i++) {
+            str += " "
+        }
+        return str
+    }
+}
+
+const u = new JLogger()
+
+function box_contains(root: any, position: Point) {
+    let r = new Rect(root.position.x,root.position.y, root.size.w, root.size.h)
+    return r.contains(position)
+}
+
+function find_box(root: RootBox, position: Point):any {
+    if(box_contains(root,position)) {
+        let pos2 = position.subtract(root.position)
+        for(let i=0; i<root.blocks.length; i++) {
+            let blk = root.blocks[i]
+            if(box_contains(blk,pos2)) {
+                let pos3 = pos2.subtract(blk.position)
+                for(let j=0; j<blk.lines.length; j++) {
+                    let line = blk.lines[j]
+                    if(box_contains(line,pos3)) {
+                        return line
+                    }
+                }
+                return blk
+            }
+        }
+        return root
+    }
+    return null
+}
+
+function do_render(root: RootBox, g: SurfaceContext, selected_box: any) {
+    let pos = root.position as Point
+    let size = root.size as Size
+    //@ts-ignore
+    let ctx:CanvasRenderingContext2D = g.ctx
+    ctx.fillStyle =root.style.background_color
+    ctx.fillRect(pos.x, pos.y, size.w, size.h)
+    ctx.save()
+    ctx.translate(pos.x, pos.y)
+
+    function stroke_box(g: SurfaceContext, blk:any, color:string) {
+        let pos = blk.position as Point
+        let size = blk.size as Size
+        g.stroke(new Rect(pos.x,pos.y,size.w,size.h),color)
+    }
+
+    root.blocks.forEach(blk => {
+        let pos = blk.position as Point
+        let size = blk.size as Size
+        if(blk.style.border_width > 0) {
+            let r = new Rect(pos.x,pos.y,size.w,size.h)
+            g.fill(r, blk.style.border_color)
+            g.fill(r.shrink(blk.style.border_width*2),blk.style.background_color)
+        } else {
+            ctx.fillStyle = blk.style.background_color
+            ctx.fillRect(pos.x, pos.y, size.w, size.h)
+        }
+        ctx.save()
+        ctx.translate(pos.x, pos.y)
+        blk.lines.forEach((ln:LineBox) => {
+            let pos = ln.position as Point
+            ctx.save()
+            ctx.translate(pos.x, pos.y);
+            ln.spans.forEach((spn:SpanBox) => {
+                let pos = spn.position
+                let font = (spn.style.weight === 'bold')?'bold':'base'
+                g.fillStandardText(spn.text, pos.x, pos.y, font)
+                if(spn.style.underline) {
+                    g.fill(new Rect(pos.x,pos.y,ln.size.w,2), spn.style.color)
+                }
+            })
+            ctx.restore()
+            if(ln === selected_box) {
+                stroke_box(g,ln, 'red')
+            }
+        })
+        ctx.restore()
+        if(blk === selected_box) {
+            stroke_box(g,blk, 'red')
+        }
+    })
+    ctx.restore()
+}
+
+export class RichTextArea extends BaseView {
+    _doc: Paragraph[]
+    render_tree_root: any
+    private selected_box: any;
+
+    constructor() {
+        super("rich-text-area");
+        this._doc = []
+    }
+
+    doc(): Paragraph[] {
+        return this._doc
+    }
+
+    set_doc(doc: Paragraph[]) {
+        this._doc = doc
+    }
+
+    override input(event: CoolEvent) {
+        if(event.type === POINTER_MOVE) {
+            let e = event as PointerEvent
+            this.selected_box = find_box(this.render_tree_root as RootBox, e.position)
+            event.ctx.repaint()
+        }
+    }
+
+    draw(g: SurfaceContext): void {
+        g.fillBackgroundSize(this.size(), 'white')
+        do_render(this.render_tree_root, g, this.selected_box)
+    }
+
+    layout(g: SurfaceContext, available: Size): Size {
+        if(this.size().w !== available.w) {
+            this.set_size(new Size(available.w, available.h))
+            this.render_tree_root = do_layout(this._doc, this.size(), g)
+            this.set_size(new Size(this.size().w,this.render_tree_root.size.h))
+        }
+        return this.size()
+    }
+}
